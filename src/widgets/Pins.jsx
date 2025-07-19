2ffdd636b614303d4bac8700fbedd7c6dbf9d1a5
Rudra Singh Bhau <rudrabhau844@gmail.com>
2025-07-19 19:39:57 +0530

    Update Pins.jsx

--- src/widgets/Pins.jsx
+++ src/widgets/Pins.jsx
@@ -1,11 +1,16 @@
 import { useState } from "react";
-import { Box, IconButton } from "@mui/material";
+import { Box, IconButton, useTheme } from "@mui/material";
 import RotateLeftOutlinedIcon from "@mui/icons-material/RotateLeftOutlined";
+import { tokens } from "../theme";
+
 
 const images = import.meta.glob("../assets/pins/*.jpg", { eager: true });
 const imageUrls = Object.values(images).map((img) => img.default);
 
 function ImagePins() {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    
     const [index, setIndex] = useState(0);
 
     const showNext = () => {
@@ -15,7 +20,7 @@ function ImagePins() {
 
     return (
         <>
-            <Box className="pins-wrapper" display="flex" padding="15px" borderRadius="10px" bgcolor={colors.primary[400]} width="100%" position="relative">
+            <Box className="pins-wrapper" display="flex" height="180.35px" padding="15px" borderRadius="10px" bgcolor={colors.primary[400]} width="100%" position="relative">
                 <img src={imageUrls[index]} alt="pin" width="100%" height="auto" className="pins-img" style={{ borderRadius: "8px", objectFit: "contain" , pointerEvents:"none"}} />
                 <IconButton className="random-img-btn" sx={{ padding: "5px", position: "absolute", right: '10%', bottom: "10%", color: "#efefef" }} size="20" onClick={showNext}>
                     <RotateLeftOutlinedIcon />
