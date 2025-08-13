f86462225a294563d3a69e8f82c864ad15c60af4
Rudra Singh Bhau <rudrabhau844@gmail.com>
2025-08-13 22:08:13 +0530

    Create Github.jsx

---
+++ src/scenes/Github.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import Header from "./global/Header";
+import { tokens } from "../theme";
+import { Box, useTheme, Typography, IconButton, Divider } from "@mui/material";
+import TabOutlinedIcon from '@mui/icons-material/TabOutlined';
+import StarBorderOutlinedIcon from '@mui/icons-material/StarBorderOutlined';
+import ForkRightOutlinedIcon from '@mui/icons-material/ForkRightOutlined';
+import { useState, useEffect } from "react";
+
+function App() {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  
+  const [repos, setRepos] = useState([]);
+  const repos_to_show = ["Gramophone","Dashboard", "Python-Image-Steganography", "Vibranto"];
+  const username = "rudee-Sb";
+  
+  useEffect(() => {
+    async function fetch_repos() {
+      const promises = repos_to_show.map(repo =>
+        fetch(`https://api.github.com/repos/${username}/${repo}`)
+          .then(res => res.json())
+      );
+
+      const data = await Promise.all(promises);
+      setRepos(data);
+    }
+
+    fetch_repos();
+  }, []);
+  
+  useEffect(() => {
+    console.log("repo data =", repos);
+  },[repos])
+    
+  return (
+    <>
+      <Box m="20px">
+        <Header title="GITHUB" subtitle="Graveyard of Ideas."></Header>
+        <Box display="grid" gridTemplateColumns="repeat(12, 1fr)" gridAutoRows="140px" gap="20px" >
+          // row 1
+          {
+            repos.map(repo => (
+            <Box key={repo.id} gridColumn="span 3"  borderRadius="10px" padding="10px" display="flex" alignItems="center" justifyContent="center" sx={{ cursor: "pointer" }} bgcolor="rgb(67, 73, 87)" p="15px">
+              <Box display="flex" width="100%" alignItems="center" flexDirection="row" gap="15px">
+                <TabOutlinedIcon />
+                <Typography variant="h4" fontWeight="bold" color="rgb(146 227 233)">{repo.name}</Typography>
+               <Typography
+                  variant="h5"
+                  sx={{
+                    color: "rgb(244, 244, 244)",
+                    background: "transparent",
+                    padding: "6px 8px",
+                    border: "1.4px solid rgb(244, 244, 244)",
+                    borderRadius: "12px"
+                  }}
+                >
+                  {repo.private ? "private" : "public"}
+                </Typography>
+              </Box>
+            </Box>
+            ))
+          }
+        </Box>
+      </Box>
+    </>
+  )
+}
+
+export default App
