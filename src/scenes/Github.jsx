aad7d481b5c6bd984cd140ed25656cbed2ce9ad1
Rudra Singh Bhau <rudrabhau844@gmail.com>
2025-08-13 23:10:17 +0530

    Added github contri graph along with layout changes

--- src/scenes/Github.jsx
+++ src/scenes/Github.jsx
@@ -6,6 +6,8 @@ import TabOutlinedIcon from '@mui/icons-material/TabOutlined';
 import StarBorderOutlinedIcon from '@mui/icons-material/StarBorderOutlined';
 import ForkRightOutlinedIcon from '@mui/icons-material/ForkRightOutlined';
 import { useState, useEffect } from "react";
+import ContriGraph from "../widgets/GithubContri";
+
 
 function Github() {
   const theme = useTheme();
@@ -38,11 +40,12 @@ function Github() {
       <Box m="20px">
         <Header title="GITHUB" subtitle="Graveyard of Ideas."></Header>
         <Box display="grid" gridTemplateColumns="repeat(12, 1fr)" gridAutoRows="140px" gap="20px" >
+          {/* Row 1 */}
           {
             repos.map(repo => (
-            <Box key={repo.id} gridColumn="span 3"  borderRadius="10px" border="1.4px solid rgb(67, 73, 87)" padding="10px" display="flex" flexDirection="column" alignItems="center" justifyContent="center" bgcolor="transparent" p="15px">
-              <Box display="flex" width="100%" alignItems="center" justifyConetnt="space-between" flexDirection="row" gap="15px">
-                <Box display="flex" alignItems="center">
+            <Box key={repo.id} gridColumn="span 3"  borderRadius="10px" border="1.4px solid rgb(67, 73, 87)" padding="10px" display="flex" flexDirection="column" gap="20px" justifyContent="center" bgcolor="transparent" p="15px">
+              <Box display="flex" width="100%" alignItems="center" justifyContent="space-between" flexDirection="row" gap="15px">
+                <Box display="flex" alignItems="center" gap="15px">
                   <TabOutlinedIcon />
                   <Typography variant="h4" fontWeight="bold" color="rgb(146 227 233)" onClick={() => window.open(repo.html_url, "_blank")} sx={{ cursor: "pointer" }}>{repo.name}</Typography>
                 </Box>
@@ -68,6 +71,10 @@ function Github() {
             </Box>
             ))
           }
+          {/* Row 2 */}
+          <Box gridColumn="span 7" height="225px" >
+              <ContriGraph ></ContriGraph>
+          </Box>
         </Box>
       </Box>
     </>
